bd2f9903facf3f9edd35b608c01370deddd5b6a3
FranciscoPerera <franciscopneto2017@gmail.com>
2025-11-26 23:33:43 +0000

    SuperMelhoria

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-alert('Ola, foi carregado o script!');
+// Vamos criar os elementos do html com o javascript
+
+function criarItemCardapio(titulo, descricao, preco, foto) {
+
+    const divCardapio = document.createElement('div');
+    divCardapio.className = 'item-cardapio';
+
+    const h3Titulo = document.createElement('h3');
+    h3Titulo.textContent = titulo;
+
+    const pDescricao = document.createElement('p');
+    pDescricao.textContent = descricao;
+    pDescricao.className = 'descricao';
+
+    const pPreco = document.createElement('p');
+    pPreco.textContent = 'Preço: R$ ' + preco.toFixed(2);
+
+    const img = document.createElement('img');
+    img.src = foto;
+    img.className = 'img-item';
+
+    const divC = document.getElementById('cardapio');
+
+    divCardapio.appendChild(h3Titulo);
+    divCardapio.appendChild(pDescricao);
+    divCardapio.appendChild(pPreco);
+    divCardapio.appendChild(img);
+
+    divC.appendChild(divCardapio);
+}    
+
+criarItemCardapio(
+    'Bolo De Chocolate',
+    'Bolo de chocolate com recheio de brigadeiro e cobertura de ganache. Uma delícia para os amantes de chocolate.',
+     45.00,
+    'img/2.jpeg'
+)
+
+criarItemCardapio(
+    'Bolo De Morango',
+    'Bolo com morango, calda de morango caseira e cobertura de chantilly. Perfeito para celebrações especiais.',
+     50.00,
+    'img/1.jpeg'
+)
+
+criarItemCardapio(
+    'Bolo De Cenora',
+    'Bolo de cenoura fofinho com cobertura de chocolate. Uma combinação clássica que agrada a todos.',
+     40.00,
+    'img/3.jpeg'
+)
+
+criarItemCardapio(
+    'Bolo De Limão',
+    'Bolo de limão refrescante com recheio cremoso e cobertura de merengue. Ideal para quem gosta de sabores cítricos.',
+     48.00,
+    'img/4.jpeg'
+)
+
+criarItemCardapio(
+    'Bolo Red Velvet',
+    'Bolo Red Velvet com recheio de cream cheese e cobertura de glacê. Uma escolha sofisticada para ocasiões especiais.',
+     55.00,
+    'img/5.jpeg'
+)
+
+criarItemCardapio(
+    'Bolo De Coco',
+    'Bolo de coco macio com recheio de creme de coco e cobertura de coco ralado. Uma delícia tropical.',
+     42.00,
+    'img/6.jpeg'
+)
